0e3aa33e979529928751e7ea6af98be3bea534f2
andrzej <andrzejneumann@zoho.com>
2017-09-25 00:31:29 +0200

    Added handlers' implementation and tests.

---
+++ stripe_pipeline/handlers.py
@@ -0,0 +1,88 @@
+import json
+import boto3
+import datetime as dt
+
+
+class JSONFormatException(Exception):
+    """
+    Exception representing errors in input JSON
+    like missing expected keys, etc.
+    """
+
+
+s3 = boto3.client('s3')
+
+dynamodb = boto3.client('dynamodb')
+
+
+def save_source_in_s3(event, context):
+    source = validate_and_extract(event, ('data', 'object', 'source'))
+
+    now = dt.datetime.now()
+    s3.Object('stripe_payments',
+              '{:%Y-%m-%d}/{}'.format(now,
+                                      now.timestamp())).put(Body=source)
+
+
+def put_in_dynamodb(event, context):
+    payment = validate_and_extract(event, ('data', 'object'))
+
+    dynamodb.put_item(
+        Item={
+            'PaymentId': {
+                'S': payment['id'],
+            },
+            'CustomerId': {
+                'S': payment['source']['customer'],
+            },
+            'NameOnPayment': {
+                'S': payment['source']['name'],
+            },
+            'BillingCountry': {
+                'S': payment['source']['country'],
+            },
+            'BillingAddress': {
+                'S': '{} {}'.format(
+                    payment['source']['address_line1'],
+                    payment['source']['address_line2']),
+            },
+            'BillingCity': {
+                'S': payment['source']['address_line1'],
+            },
+            'BillingZipCode': {
+                'S': payment['source']['address_zip'],
+            },
+            'PaymentType': {
+                'S': payment['source']['type'],
+            },
+            'PaymentAmount': {
+                'S': payment['amount'],
+            },
+            'PaymentDate': {
+                'S': dt.datetime.utcfromtimestamp(
+                    payment['created']).isoformat(),
+            },
+        },
+        TableName='PaymentTable',
+    )
+
+
+def validate_and_extract(json_string, path):
+    try:
+        stripe_data = json.loads(json_string)
+    except (json.decoder.JSONDecodeError, TypeError) as decoding_error:
+        raise JSONFormatException(
+            'Decoding error encountered') from decoding_error
+
+    return get_value(stripe_data, path)
+
+
+def get_value(a_dict, keys):
+    if keys[0] in a_dict:
+        if len(keys) == 1:
+            return a_dict[keys[0]]
+
+        return get_value(a_dict[keys[0]], keys[1:])
+    else:
+        raise JSONFormatException(
+            '{} not found in input JSON'.format(keys[0]))
---
+++ test/test_handlers.py
@@ -0,0 +1,40 @@
+import pytest
+from stripe_pipeline.handlers import get_value
+from stripe_pipeline.handlers import validate_and_extract
+from stripe_pipeline.handlers import JSONFormatException
+
+
+def test_get_value():
+    a_dict = {'a': {'b': {'d': 3}}}
+
+    assert get_value(a_dict, ('a', 'b', 'd')) == 3
+
+    with pytest.raises(JSONFormatException) as error:
+        get_value(a_dict, ('a', 'b', 'c'))
+    assert 'c not found in input JSON' in str(error.value)
+
+
+def test_validate_and_extract():
+    correct_json = """
+        {
+            "a": {
+                "b": {
+                    "c": 123
+                }
+            }
+        }
+    """
+
+    assert validate_and_extract(correct_json, ('a', 'b', 'c')) == 123
+
+    incorrect_json = """
+        {
+            "a": {
+                "b": {
+                    "c": 123
+            }
+        }"""
+
+    with pytest.raises(JSONFormatException) as error:
+        validate_and_extract(incorrect_json, ('a', 'b', 'c'))
+    assert 'Decoding error encountered' in str(error.value)
